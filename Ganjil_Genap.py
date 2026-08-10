b8a4c3e2e6a5816955c53133ad2a7793e6944470
Gilangg-24 <mgilangrahmana@gmail.com>
2026-08-10 14:05:41 +0700

    Add files via upload

--- Ganjil_Genap.py
+++ Ganjil_Genap.py
@@ -1,8 +1,20 @@
 # Program cek bilangan ganjil atau genap
 
-bilangan = int(input("Masukkan sebuah bilangan: "))
+for i in range(100):
+    bilangan = int(input("Masukkan bilangan: "))
 
-if bilangan % 2 == 0:
-    print("Bilangan", bilangan, "adalah bilangan genap")
-else:
-    print("Bilangan", bilangan, "adalah bilangan ganjil")
+    if bilangan % 2 == 0:
+        print("Bilangan", bilangan, "adalah bilangan genap")
+    else:
+        print("Bilangan", bilangan, "adalah bilangan ganjil")
+
+    pilihan = input("Apakah ingin mengecek lagi? (y/n): ")
+
+    if pilihan.lower() == "n":
+        print("Program berhenti.")
+        break
+
+    elif pilihan.lower() == "y":
+        continue
+    else:
+        print("Input tidak valid! Masukkan hanya y atau n.")
